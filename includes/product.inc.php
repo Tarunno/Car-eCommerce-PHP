63443abd25e480fb35ff1b64f4cedc52d9745fd0
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-10-10 10:21:39 +0600

    Update product.inc.php

--- includes/product.inc.php
+++ includes/product.inc.php
@@ -16,20 +16,17 @@
 		$products = $product->get_product($id);
 		$row = $products->fetch_assoc();
 	}
-
 	if(isset($_POST['add-to-cart'])){
 		$cart->add_to_cart($row['id'], $row['manufacturer'], $row['price'], $_SESSION['id'], $_SESSION['username'], $row['model'], $_SESSION['email'], $row['image']);
 		$user->update_cart_increment($_SESSION['id']);
-		$_SESSION['message'] = "Car added to your cart";
 		$_SESSION['cart'] += 1;
-		header("Location: product.php?id=$id");
+		header("Location: index.php");
 		exit();
 	}
 	if(isset($_POST['add-to-cart-product'])){
 		$cart->add_to_cart_product("-1", $row['manufacturer'], $row['price'], $_SESSION['id'], $_SESSION['username'], $row['model'], $_SESSION['email'], $row['image'], $row['id']);
 		$user->update_cart_increment($_SESSION['id']);
-		$_SESSION['message'] = "Product added to your cart";
 		$_SESSION['cart'] += 1;
-		header("Location: product.php?product_id=$id");
+		header("Location: index.php");
 		exit();
 	}
